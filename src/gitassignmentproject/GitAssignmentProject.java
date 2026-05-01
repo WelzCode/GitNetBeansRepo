d95cff2c8e6edb4600c808c6bbe2f6329baac380
Samkelo <wellzchirwa22@gmail.com>
2026-05-01 21:53:46 +0200

    Initial code commit

---
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitassignmentproject;
+
+/**
+ *
+ * @author wellz
+ */
+public class GitAssignmentProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to version control using Git");
+    }
+    
+}
